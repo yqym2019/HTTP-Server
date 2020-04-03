5b17c21cf50d828c27223251a19df61a12b2b71b
jinxiaolin <137185065@qq.com>
2020-04-03 14:30:31 +0800

    启动http-server服务

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const conf = require('./config/defaultConfig');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('http-server');
+});
+
+server.listen(conf.port, conf.hostname, () => {
+    const addr = `http:${conf.hostname}:${conf.port}`;
+    console.info(`Server started at ${addr}`);
+});
