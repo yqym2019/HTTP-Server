286f86960b47f5d403e0c9bc7318a0e3bee4b903
jinxiaolin <137185065@qq.com>
2020-04-03 14:18:20 +0800

    config

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "extends": [
+        "eslint:recommended"
+    ],
+    "rules": {
+        "no-console": ["error", {
+            "allow": ["warn", "error", "info"]
+        }]
+    },
+    "parser": "babel-eslint",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "script"
+    },
+    "globals": {
+        "window": true
+    },
+    "env": {
+        "node": true,
+        "es6": true,
+        "mocha": true
+    }
+};
