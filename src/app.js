6be8a06f7de7bae464ff9f5e317103581e753f4c
jinxiaolin <137185065@qq.com>
2020-04-05 21:42:21 +0800

    添加自动打开功能

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const conf = require('./config/defaultConfig');
 const route = require('./helper/route');
+const openUrl = require('./helper/openUrl');
 
 class Server {
     constructor(config) {
@@ -17,6 +18,7 @@ class Server {
         server.listen(this.conf.port, this.conf.hostname, () => {
             const addr = `http://${this.conf.hostname}:${this.conf.port}`;
             console.info(`Server started at ${addr}`);
+            openUrl(addr);
         });
     }
 }
